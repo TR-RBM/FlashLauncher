dacd966aa155905186e29b72e1d4a5f0b0d411a6
Tim <Tim@DESKTOP-TAP5DSG>
2021-10-28 05:49:05 +0200

    + new Class: AccountManager

---
+++ DCUO Flash Launcher/scripts/AccountManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FlashLauncher
+{
+    /// <summary>
+    /// Class Object for managing the accounts in the local sqlite database
+    /// </summary>
+    internal class AccountManager
+    {
+        /// <summary>
+        /// list that stores the accounts while reading and writeing to the database
+        /// </summary>
+        private List<Account> accounts = new();
+
+        internal AccountManager()
+        {
+        }
+
+        /// <summary>
+        /// Returns a list of all accounts in the local database
+        /// </summary>
+        /// <returns>Accounts[]</returns>
+        internal Account[] GetAccountList()
+        {
+            return new Account[] {};
+        }
+    }
+}
