9e8f06faa67e7623512a0cc29ab827cdef1b0b57
Tim <Tim@DESKTOP-TAP5DSG>
2021-11-04 22:46:07 +0100

    + PlaySession class

---
+++ DCUO Flash Launcher/scripts/PlaySession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashLauncher
+{
+    public class PlaySession
+    {
+        public string category = new("");
+        public string result = new("");
+        public string error = new("");
+        public string username = new("");
+        public string launchArgs = new("");
+    }
+}
